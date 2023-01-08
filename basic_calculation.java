d561a969fa54feddb2f41f07276776326b1678b2
SRIGANTH R <82374448+sriganth143@users.noreply.github.com>
2023-01-08 16:06:59 +0000

    Add files via upload

---
+++ basic_calculation.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class basic_calculation{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter the value of A : ");
+        a = sc.nextInt();
+        System.out.println("Enter the value of B : ");
+        b = sc.nextInt();
+        System.out.println("--------Addition--------");
+        c = a+b;
+        System.out.println(c);
+        System.out.println("------Subtraction-------");
+        c = a-b;
+        System.out.println(c);
+        System.out.println("-------Mutiplication--------");
+        c = a*b;
+        System.out.println(c);
+        System.out.println("--------Division--------");
+        c = a/b;
+        System.out.println(c);
+    
+    }
+}
+
+
+
+
+
+
