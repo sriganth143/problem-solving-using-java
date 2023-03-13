f70d985f7b6bf94a495f6b5593ae8f0428c1a000
SRIGANTH R <82374448+sriganth143@users.noreply.github.com>
2023-03-13 16:13:18 +0000

    Wonder work magic show sloved

--- operator and expressions/Wonder_work_magic_show.java
+++ operator and expressions/Wonder_work_magic_show.java
@@ -1,4 +1,15 @@
+import java.util.*;
 public class Wonder_work_magic_show{
-    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        float r1=sc.nextFloat();
+        int n2=sc.nextInt();
+        float r2=sc.nextFloat();
+        int n3=sc.nextInt();
+        float r3=sc.nextFloat();
+        float output=(n1*r1+n2*r2+n3*r3)/(n1+n2+n3);
+        System.out.println(output);
+    }
 
 }
